b545ba12b61d82706a6b91d698f36d8babfec684
Dzeno-xiyan <Dzenoduric1986@gmail.com>
2024-11-07 17:03:18 +0100

    Create forschleife.java

---
+++ Schleifen/forschleife.java
@@ -0,0 +1,14 @@
+3. for-Schleife
+Die for-Schleife ist oft für Zählschleifen geeignet, bei denen eine bestimmte Anzahl von Wiederholungen bekannt ist.
+
+Java
+for (initialisierung; bedingung; inkrement) {
+    // Code, der wiederholt ausgeführt wird
+}
+Verwende den Code mit Vorsicht.
+
+Beispiel:
+
+for (int i = 0; i < 10; i++) {
+    System.out.println("i: " + i);
+}
