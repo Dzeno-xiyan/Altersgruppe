fc8d7c6c466522f9226199815205fc04c6f19cd0
Dzeno-xiyan <Dzenoduric1986@gmail.com>
2024-11-07 12:29:54 +0100

    Update MusterSchleifenfor.java

--- MusterSchleifenfor.java
+++ MusterSchleifenfor.java
@@ -1,3 +1,35 @@
+/*
+ * Geben Sie folgende Konsolenausgaben durch die Verwendung von geschachtelten Schleifen aus:
+
+a)
+1 2 3 4
+1 2 3 4
+1 2 3 4
+
+b) 
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+c)
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+
+d) (Fortgeschritten)
+12345
+  1234
+     123
+      12
+        1
+        */
+
+
 package Scanner;
 
 import java.util.Scanner;
@@ -5,7 +37,7 @@ import java.util.Scanner;
 public class MusterAufgaben1 {
     public static void main(String[] args) {
         int rows = 5; // Anzahl der Zeilen
-        int rows1 = 4; // 2x Delaration
+        int rows1 = 4; // 2x dekla für muster a
         int cols = 5; // Maximale Anzahl der Spalten
 
         // Muster a)
@@ -41,7 +73,7 @@ public class MusterAufgaben1 {
         for (int i = rows; i >= 1; i--) {
             for (int j = 1; j <= cols; j++) {
                 if (j <= rows - i) {
-                    System.out.print("  "); // Zwei Leerzeichen für den Versatz
+                    System.out.print("  "); // Leeerzeichen zum verschieben
                 } else {
                     System.out.print(j - (rows - i) + " ");
                 }
