5ea38c3ab4d709caf1c8d8b552fa0d1b35143aae
Dzeno-xiyan <Dzenoduric1986@gmail.com>
2024-11-07 16:58:35 +0100

    Create If Else Schleife.java

---
+++ Schleifen/If Else Schleife.java
@@ -0,0 +1,26 @@
+public class IfElseBeispiel {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        // Vergleich von a und b
+        if (a > b) {
+            System.out.println("a ist größer als b");
+        } else if (a < b) {
+            System.out.println("a ist kleiner als b");
+        } else {
+            System.out.println("a ist gleich   
+ b");
+        }
+
+        // Vergleich von Zeichen
+        char char1 = 'a';
+        char char2 = 'b';
+
+        if (char1 == char2) {
+            System.out.println("Die Zeichen sind gleich");
+        } else {
+            System.out.println("Die Zeichen sind unterschiedlich");
+        }
+    }
+}
