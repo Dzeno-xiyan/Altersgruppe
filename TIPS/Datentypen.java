1d939b8355af81c6484ed027f5d5cd2ad6485484
Dzeno-xiyan <Dzenoduric1986@gmail.com>
2024-11-07 17:08:29 +0100

    Create Datentypen.java

---
+++ TIPS/Datentypen.java
@@ -0,0 +1,31 @@
+Datentypen: Die Bausteine der Programmierung
+
+Datentypen definieren, welche Art von Daten eine Variable speichern kann. Sie legen fest, wie diese Daten im Speicher repräsentiert werden und welche Operationen auf ihnen ausgeführt werden können.
+
+Grundlegende Datentypen in Java
+
+Java unterscheidet zwischen zwei Hauptkategorien von Datentypen:
+
+Primitive Datentypen:
+
+Numerische Typen:
+Ganzzahlen: byte, short, int, long (für verschiedene Größenbereiche)
+Gleitkommazahlen: float, double (für Zahlen mit Komma)
+Zeichen: char (für einzelne Zeichen)
+Boolesch: boolean (für Wahrheitswerte: true oder false)
+Referenztypen:
+
+Klassen: Repräsentieren Objekte, die Eigenschaften (Attribute) und Verhalten (Methoden) haben.
+Interfaces: Definieren einen Vertrag, den Klassen erfüllen müssen.
+Arrays: Sammeln von Elementen des gleichen Datentyps.
+Übersichtstabelle:
+
+Datentyp	Beschreibung	Beispiel
+byte	Kleine ganze Zahl	byte age = 30;
+short	Mittlere ganze Zahl	short year = 2023;
+int	Ganze Zahl	int population = 1000000;
+long	Große ganze Zahl	long distance = 9223372036854775807L;
+float	Gleitkommazahl mit einfacher Genauigkeit	float pi = 3.14f;
+double	Gleitkommazahl mit doppelter Genauigkeit	double radius = 6.371;
+char	Einzelnes Zeichen	char firstLetter = 'A';
+boolean	Wahrheitswert	boolean isSunny = true;
