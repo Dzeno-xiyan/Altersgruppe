56df90ccd6e4dea9a3057ce2a686dfaf93ed0c46
Dzeno-xiyan <Dzenoduric1986@gmail.com>
2024-11-07 17:01:32 +0100

    Create DoWhile.java

---
+++ Schleifen/DoWhile.java
@@ -0,0 +1,10 @@
+public class DoWhileSchleifeBeispiel {
+    public static void main(String[] args) {
+        int zaehler = 0;
+
+        do {
+            System.out.println("Der Zähler ist: " + zaehler);
+            zaehler++;
+        } while (zaehler < 5);
+    }
+}
