84bcd1b5ae275ca7718dbdbac4e5fa117ee5ee80
Dzeno-xiyan <Dzenoduric1986@gmail.com>
2024-11-07 17:01:12 +0100

    Create WhileSchleife.java

---
+++ Schleifen/WhileSchleife.java
@@ -0,0 +1,10 @@
+public class WhileSchleifeBeispiel {
+    public static void main(String[] args) {
+        int zaehler = 0;
+
+        while (zaehler < 5) {
+            System.out.println("Der Zähler ist: " + zaehler);
+            zaehler++;
+        }
+    }
+}
