4f05703311496593f375d0fe147c46fc5844845a
Dzeno-xiyan <Dzenoduric1986@gmail.com>
2024-11-11 14:13:53 +0100

    Create LuckyObjeMetho.java

---
+++ Methoden/LuckyObjeMetho.java
@@ -0,0 +1,35 @@
+package Scanner;
+
+public class LuckyObjekMetho {
+
+    public static boolean isLucky(int ticketNumber) {
+        // Umwandlung der Zahl in einen String für eine einfachere Bearbeitung
+        String ticketString = String.valueOf(ticketNumber);
+
+        // Überprüfung, geradee oder ungerade
+        if (ticketString.length() % 2 != 0) {
+            return false; // ungerade kein lucky ticket
+        }
+
+        int middleIndex = ticketString.length() / 2; // Mitte berechnen
+        int sumFirstHalf = 0;
+        int sumSecondHalf = 0;
+
+     // Berechnung der Summe erste und zweite hälfte
+        for (int i = 0; i < middleIndex; i++) { // bei 0 Anfangen und hochzählen bis i nicht mehr kleiner die Mitte ist.
+            sumFirstHalf += Character.getNumericValue(ticketString.charAt(i)); // Char in Nummer umwandeln 
+            sumSecondHalf += Character.getNumericValue(ticketString.charAt(i + middleIndex)); // aber der zweiten Hälfte aus dem i + middle extrahieren
+        }
+
+        return sumFirstHalf == sumSecondHalf; // erste und zweite gleich LUCKY
+    }
+
+    public static void main(String[] args) {
+        int ticketNumber = 411222;
+        if (isLucky(ticketNumber)) {
+            System.out.println("Das Ticket ist lucky!");
+        } else {
+            System.out.println("Das Ticket Unlucky");
+        }
+    }
+}
